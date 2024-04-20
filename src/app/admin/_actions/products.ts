a8affeb2270084afa73a35ab43daae8e89536116
etan094 <etan094@gmail.com>
2024-04-19 17:06:01 -0700

    deleted files using fs on delete.

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -56,10 +56,16 @@ export async function toggleProductAvailability(
   id: string,
   isAvailableForPurchase: boolean
 ) {
-  await db.product.update({ where: { id }, data: { isAvailableForPurchase } });
+  await db.product.update({
+    where: { id },
+    data: { isAvailableForPurchase },
+  });
 }
 
 export async function deleteProduct(id: string) {
   const product = await db.product.delete({ where: { id } });
   if (!product) return notFound();
+
+  await fs.unlink(product.filePath);
+  await fs.unlink(`public/${product.imagePath}`);
 }
