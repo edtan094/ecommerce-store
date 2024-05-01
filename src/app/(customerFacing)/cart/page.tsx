1568b6a32743b5b1e9cb51aad9227e79534ce27e
etan094 <etan094@gmail.com>
2024-04-30 22:54:19 -0700

    Working on cart page.

--- src/app/(customerFacing)/cart/page.tsx
+++ src/app/(customerFacing)/cart/page.tsx
@@ -2,6 +2,16 @@
 
 import { useEffect, useState } from "react";
 import { CartItem } from "./_components/CartItem";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { formatCurrency } from "@/lib/formatters";
 
 export default function MyCartPage() {
   const [cartItems, setCartItems] = useState<
@@ -35,31 +45,55 @@ export default function MyCartPage() {
   }, []);
 
   return (
-    <main>
-      <div className="flex justify-center">
-        <h1 className="text-4xl">My Cart</h1>
-      </div>
-      {cartItems.length > 0 ? (
-        <div className="my-9">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-4">
-            {cartItems.map((item) => {
-              return (
-                <CartItem
-                  key={item.id}
-                  id={item.id}
-                  name={item.name}
-                  priceInCents={item.priceInCents}
-                  description={item.description}
-                  imagePath={item.imagePath}
-                  handleRemoveCartItem={handleRemoveCartItem}
-                />
-              );
-            })}
-          </div>
+    <main className="flex">
+      <div className="w-2/3 px-4">
+        <div className="flex justify-center">
+          <h1 className="text-4xl">My Cart</h1>
         </div>
-      ) : (
-        <h2 className="text-2xl">Your Cart is Empty!</h2>
-      )}
+        {cartItems.length > 0 ? (
+          <div className="my-9">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-4">
+              {cartItems.map((item) => {
+                return (
+                  <CartItem
+                    key={item.id}
+                    id={item.id}
+                    name={item.name}
+                    priceInCents={item.priceInCents}
+                    description={item.description}
+                    imagePath={item.imagePath}
+                    handleRemoveCartItem={handleRemoveCartItem}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          <h2 className="text-2xl">Your Cart is Empty!</h2>
+        )}
+      </div>
+      <div className="w-1/3">
+        <Card>
+          <CardHeader>
+            <CardTitle>Order Summary</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-lg">Items in Cart: {cartItems.length}</p>
+            <p className="text-lg">
+              Total:{" "}
+              {formatCurrency(
+                cartItems.reduce((acc, item) => acc + item.priceInCents, 0) /
+                  100
+              )}
+            </p>
+          </CardContent>
+          <CardFooter>
+            <Button>
+              <Link href="/checkout">Proceed to Checkout</Link>
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
     </main>
   );
 }
