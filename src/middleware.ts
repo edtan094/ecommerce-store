7dfc568acca2368a9866073573cc0603b591ce13
etan094 <etan094@gmail.com>
2024-05-22 23:01:32 -0700

    temporarily removed middleware.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,34 @@
-import { NextRequest, NextResponse } from "next/server";
-import { isValidPassword } from "./lib/isValidPassword";
+// import { NextRequest, NextResponse } from "next/server";
+// import { isValidPassword } from "./lib/isValidPassword";
 
-export async function middleware(req: NextRequest) {
-  if ((await isAuthenticated(req)) === false) {
-    return new NextResponse("Unauthorized", {
-      status: 401,
-      headers: { "WWW-Authenticate": "Basic" },
-    });
-  }
-}
+// export async function middleware(req: NextRequest) {
+//   if ((await isAuthenticated(req)) === false) {
+//     return new NextResponse("Unauthorized", {
+//       status: 401,
+//       headers: { "WWW-Authenticate": "Basic" },
+//     });
+//   }
+// }
 
-async function isAuthenticated(req: NextRequest) {
-  const authHeader =
-    req.headers.get("authorization") || req.headers.get("Authorization");
+// async function isAuthenticated(req: NextRequest) {
+//   const authHeader =
+//     req.headers.get("authorization") || req.headers.get("Authorization");
 
-  if (!authHeader) return false;
+//   if (!authHeader) return false;
 
-  const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
+//   const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
+//     .toString()
+//     .split(":");
 
-  return (
-    username === process.env.ADMIN_USERNAME &&
-    (await isValidPassword(
-      password,
-      process.env.HASHED_ADMIN_PASSWORD as string
-    ))
-  );
-}
+//   return (
+//     username === process.env.ADMIN_USERNAME &&
+//     (await isValidPassword(
+//       password,
+//       process.env.HASHED_ADMIN_PASSWORD as string
+//     ))
+//   );
+// }
 
-export const config = {
-  matcher: "/admin/:path*",
-};
+// export const config = {
+//   matcher: "/admin/:path*",
+// };
