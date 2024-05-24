1db05e1a4bbc134e0e58301d4aa113bbd458ee1c
etan094 <etan094@gmail.com>
2024-05-23 17:08:35 -0700

    Added caching.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -7,63 +7,106 @@ import {
 } from "@/components/ui/card";
 import db from "@/db/db";
 import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { cache } from "@/lib/cache";
 
-async function getSalesData() {
-  const data = await db.order.aggregate({
-    _sum: { pricePaidInCents: true },
-    _count: true,
-  });
+// async function getSalesData() {
+//   const data = await db.order.aggregate({
+//     _sum: { pricePaidInCents: true },
+//     _count: true,
+//   });
 
-  return {
-    amount: (data._sum.pricePaidInCents || 0) / 100,
-    numberOfSales: data._count,
-  };
-}
+//   return {
+//     amount: (data._sum.pricePaidInCents || 0) / 100,
+//     numberOfSales: data._count,
+//   };
+// }
 
-async function getUserData() {
-  const [userCount, orderData] = await Promise.all([
-    db.user.count(),
-    db.order.aggregate({
+const getSalesData = cache(
+  async () => {
+    const data = await db.order.aggregate({
       _sum: { pricePaidInCents: true },
-    }),
-  ]);
+      _count: true,
+    });
 
-  return {
-    userCount,
-    averageValuePerUser:
-      userCount === 0
-        ? 0
-        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
-  };
-}
+    return {
+      amount: (data._sum.pricePaidInCents || 0) / 100,
+      numberOfSales: data._count,
+    };
+  },
+  ["/admin", "getSalesData"],
+  { revalidate: 60 }
+);
 
-async function getProductData() {
-  // const [activeCount, inactiveCount] = await Promise.all([
-  //   db.product.count({ where: { isAvailableForPurchase: true } }),
-  //   db.product.count({ where: { isAvailableForPurchase: false } }),
-  // ]);
+// async function getUserData() {
+//   const [userCount, orderData] = await Promise.all([
+//     db.user.count(),
+//     db.order.aggregate({
+//       _sum: { pricePaidInCents: true },
+//     }),
+//   ]);
 
-  // return { activeCount, inactiveCount };
+//   return {
+//     userCount,
+//     averageValuePerUser:
+//       userCount === 0
+//         ? 0
+//         : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+//   };
+// }
 
-  const product = await db.product.findMany({
-    where: { isAvailableForPurchase: true },
-    orderBy: { orders: { _count: "desc" } },
-  });
-  return product;
-}
+const getUserData = cache(
+  async () => {
+    const [userCount, orderData] = await Promise.all([
+      db.user.count(),
+      db.order.aggregate({
+        _sum: { pricePaidInCents: true },
+      }),
+    ]);
+
+    return {
+      userCount,
+      averageValuePerUser:
+        userCount === 0
+          ? 0
+          : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+    };
+  },
+  ["/admin", "getUserData"],
+  { revalidate: 60 }
+);
+
+// async function getProductData() {
+//   const [activeCount, inactiveCount] = await Promise.all([
+//     db.product.count({ where: { isAvailableForPurchase: true } }),
+//     db.product.count({ where: { isAvailableForPurchase: false } }),
+//   ]);
+
+//   return { activeCount, inactiveCount };
+// }
+
+const getProductData = cache(
+  async () => {
+    const [activeCount, inactiveCount] = await Promise.all([
+      db.product.count({ where: { isAvailableForPurchase: true } }),
+      db.product.count({ where: { isAvailableForPurchase: false } }),
+    ]);
+
+    return { activeCount, inactiveCount };
+  },
+  ["/admin", "getProductData"],
+  { revalidate: 60 }
+);
 
 export default async function AdminDashboard() {
-  // const [salesData, userData, productData] = await Promise.all([
-  //   getSalesData(),
-  //   getUserData(),
-  //   getProductData(),
-  // ]);
-
-  const productData = await getProductData();
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ]);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {/* <DashboardCard
+      <DashboardCard
         title="Sales"
         subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
         body={formatCurrency(salesData.amount)}
@@ -79,15 +122,6 @@ export default async function AdminDashboard() {
         title="Active Products"
         subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
         body={formatNumber(productData.activeCount)}
-      /> */}
-      <DashboardCard
-        title="Active Products"
-        subtitle={`Active`}
-        body={productData
-          .map((product) => {
-            return `${product.name} - ${product.priceInCents} - ${product.description} - ${product.createdAt}`;
-          })
-          .join(", ")}
       />
     </div>
   );
