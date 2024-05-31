2371415636950023c5a4d41f950fb5f7aabdb332
etan094 <etan094@gmail.com>
2024-05-31 15:04:49 -0700

    Added meme mart logo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,14 +6,25 @@ import { usePathname } from "next/navigation";
 import { ComponentProps } from "react";
 import ThemeToggle from "./ThemeToggle";
 import { ShoppingCartIcon } from "lucide-react";
+import Image from "next/image";
+import MemeMartIcon from "../app/favicon.ico";
+import { Fredoka } from "next/font/google";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+
+const fredoka = Fredoka({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Nav({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <nav className=" bg-primary-foreground text-secondary-foreground flex justify-around">
+    <nav className=" bg-primary-foreground text-secondary-foreground flex justify-around px-5 sm:px-0">
       {/* <ThemeToggle /> */}
-      <div></div>
+      <MemeMartLogo />
       <div className="flex justify-center px-4">{children}</div>
       <div className=" flex items-center">
         <Link href="/cart">
@@ -34,5 +45,23 @@ export function NavLink(props: ComponentProps<typeof Link>) {
         pathName === props.href && "bg-background text-foreground"
       )}
     />
+  );
+}
+
+export function MemeMartLogo() {
+  const router = useRouter();
+  return (
+    <div className="flex items-center">
+      <Button
+        variant="ghost"
+        className="flex items-center"
+        onClick={() => router.push("/")}
+      >
+        <span className={`${fredoka.className} font-bold sm:block hidden`}>
+          Meme Mart
+        </span>
+        <Image src={MemeMartIcon} height={32} width={32} alt="logo" />
+      </Button>
+    </div>
   );
 }
